a181cd255a11d7b0843fbb8f06e3e65c134124d8
LouisRvl <91453783+LouisRvlE@users.noreply.github.com>
2025-10-15 10:51:40 +0200

    Refactor ResultComponent: update player name handling and improve code formatting

--- src/app/quiz/result/result.component.ts
+++ src/app/quiz/result/result.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService } from "../../shared/services/quiz.service";
-import { Router } from "@angular/router";
+import { QuizService } from '../../shared/services/quiz.service';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss'],
-  standalone: false
+  standalone: false,
 })
 export class ResultComponent implements OnInit {
   score = 0;
-  scoreTotal = this.quizService.quizContent.length;
-  playerName = this.quizService.playerName;
 
-  constructor(private quizService: QuizService, private router: Router) { }
+  constructor(
+    private quizService: QuizService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.quizService.checkAnswers();
@@ -25,8 +28,19 @@ export class ResultComponent implements OnInit {
     this.quizService.resetQuiz();
   }
 
+  get name() {
+    return this.authService.user?.username || 'Anonyme';
+  }
+
+  get scoreTotal(): number {
+    return this.quizService.quizContent.filter(
+      (q) => q.category === this.quizService.categoryName
+    ).length;
+  }
+
   getGifUrl() {
-    if (this.score > this.scoreTotal/2) return 'https://media.giphy.com/media/YRuFixSNWFVcXaxpmX/giphy.gif';
+    if (this.score > this.scoreTotal / 2)
+      return 'https://media.giphy.com/media/YRuFixSNWFVcXaxpmX/giphy.gif';
     return 'https://media.giphy.com/media/jWcypagX0tNtiup1pg/giphy.gif';
   }
 }
